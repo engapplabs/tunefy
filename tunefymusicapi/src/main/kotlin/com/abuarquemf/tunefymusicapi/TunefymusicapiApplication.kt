8e1a5e57cffb9f20774fbd444da2b5d5bea73f64
ABuarque <abuarquemf@gmail.com>
2017-12-04 22:47:46 -0300

    started music api

---
+++ tunefymusicapi/src/main/kotlin/com/abuarquemf/tunefymusicapi/TunefymusicapiApplication.kt
@@ -0,0 +1,11 @@
+package com.abuarquemf.tunefymusicapi
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TunefymusicapiApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(TunefymusicapiApplication::class.java, *args)
+}
