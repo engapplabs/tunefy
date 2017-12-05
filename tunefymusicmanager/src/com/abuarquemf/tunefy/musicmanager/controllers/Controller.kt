92b7d92fb784afeca1dcfce2fb0ecfa1e0e556f7
ABuarque <abuarquemf@gmail.com>
2017-12-05 02:14:46 -0300

    implemented onaction

--- tunefymusicmanager/src/com/abuarquemf/tunefy/musicmanager/controllers/Controller.kt
+++ tunefymusicmanager/src/com/abuarquemf/tunefy/musicmanager/controllers/Controller.kt
@@ -1,8 +1,13 @@
 package com.abuarquemf.tunefy.musicmanager.controllers
 
+import javafx.event.ActionEvent
 import javafx.event.Event
 import javafx.fxml.FXML
 
 internal class Controller {
 
+    @FXML
+    fun signInAction(event: Event) {
+        println("HOHOIA")
+    }
 }
