2f8685d5cf267c6fc11e9ee2c48a0e56f6e02e92
ABuarque <abuarquemf@gmail.com>
2017-12-05 00:00:19 -0300

    added music repository

--- tunefymusicapi/src/main/kotlin/com/abuarquemf/tunefymusicapi/respositories/MusicRepository.kt
+++ tunefymusicapi/src/main/kotlin/com/abuarquemf/tunefymusicapi/respositories/MusicRepository.kt
@@ -3,4 +3,10 @@ package com.abuarquemf.tunefymusicapi.respositories
 import com.abuarquemf.tunefymusicapi.models.Music
 import org.springframework.data.mongodb.repository.MongoRepository
 
-interface MusicRepository : MongoRepository<Music, Long>
+interface MusicRepository : MongoRepository<Music, Long> {
+
+    fun findByBand(band: String): List<Music>
+
+    fun findByName(name: String) : Music
+
+}
