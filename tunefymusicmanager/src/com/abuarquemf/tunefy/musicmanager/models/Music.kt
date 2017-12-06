2210f5c7b7592de81b6e80e746f2e7df3458e148
ABuarque <abuarquemf@gmail.com>
2017-12-06 11:40:46 -0300

    added new constructor

--- tunefymusicmanager/src/com/abuarquemf/tunefy/musicmanager/models/Music.kt
+++ tunefymusicmanager/src/com/abuarquemf/tunefy/musicmanager/models/Music.kt
@@ -4,4 +4,6 @@ data class Music(val name: String, val band: String,
                  val musicResource: String, val id: Long) {
 
     constructor() : this("music_name", "band_name", "music_resource", -505)
+
+    constructor(name: String, band: String, musicResource: String) : this(name, band, musicResource, -505)
 }
