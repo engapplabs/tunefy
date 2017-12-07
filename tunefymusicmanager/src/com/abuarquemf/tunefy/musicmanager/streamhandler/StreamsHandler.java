f753225e7d236ff7eb7ca8c341714bca57513933
ABuarque <abuarquemf@gmail.com>
2017-12-06 23:25:35 -0300

    changed stream handling files

--- tunefymusicmanager/src/com/abuarquemf/tunefy/musicmanager/streamhandler/StreamsHandler.java
+++ tunefymusicmanager/src/com/abuarquemf/tunefy/musicmanager/streamhandler/StreamsHandler.java
@@ -7,12 +7,24 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
- * Created by animal505 on 09/10/17.
+ * This abstract class is useful to get
+ * an resource path and parse it into a
+ * coded string using Apache commons Base64
+ * and to create a file from a resource encoded.
  */
+import org.apache.commons.codec.binary.Base64;
+
+import java.io.*;
+
 public class StreamsHandler {
 
-    public StreamsHandler() {}
-
+    /**
+     * It gets a resource (File, Image, text...)
+     * as an InputStream and parses it into a byte array.
+     *
+     * @param stream
+     * @return array byte
+     */
     public byte[] streamToBytes(InputStream stream) {
         try {
             byte[] bytes = new byte[1024];
@@ -27,12 +39,95 @@ public class StreamsHandler {
         return null;
     }
 
-    public String codeResource(byte[] imageByteArray) {
-        return Base64.encodeBase64URLSafeString(imageByteArray);
+    /**
+     * It gets resource path an parses it into
+     * a byte array.
+     *
+     * @param resourcePath
+     * @return resource byte array
+     */
+    public byte[] stringPathToBytes(String resourcePath) {
+        try {
+            return streamToBytes(new FileInputStream(resourcePath));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
-    public byte[] decodeResource(String imageDataString) {
-        return Base64.decodeBase64(imageDataString);
+    /**
+     * It gets a resource as string( its path) and
+     * parses it into a Apache Commons Base64 string.
+     *
+     * @param resourcePath
+     * @return Base64 encoded string
+     */
+    public String stringToBase64(String resourcePath) {
+        try {
+            byte[]musicAsByteArray = streamToBytes(
+                    new FileInputStream(new File(resourcePath)));
+            return codeResource(musicAsByteArray);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Gets a byte array, encodes it into a Base64
+     * String.
+     *
+     * @param resource as byte array
+     * @return resource encoded
+     */
+    public String codeResource(byte[] sourceByteArray) {
+        return Base64.encodeBase64URLSafeString(sourceByteArray);
+    }
+
+    /**
+     * Gets a resource encoded as a string with Base64
+     * and parses into a byte array.
+     *
+     * @param resource encoded
+     * @return resource as byte array
+     */
+    public byte[] decodeResource(String sourceDataString) {
+        return Base64.decodeBase64(sourceDataString);
+    }
+
+    /**
+     * It gets a resource as a byte array
+     * and the name (path) that this resource
+     * should have when saved and save
+     * it in memory.
+     *
+     * @param resource as byte array
+     * @param name to save resource
+     */
+    public void createResourceFile(byte[] resourceAsBytes, String resourceName) {
+        try (FileOutputStream fos = new FileOutputStream(resourceName)) {
+            fos.write(resourceAsBytes);
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * It gets a resource as a String coded with apache
+     * commons Base64 and the name (path) that this resource
+     * should have when saved.
+     *
+     * @param resource as String
+     * @param name to save resource
+     */
+    public void createResourceFile(String resourceAsString, String resourceName) {
+        try (FileOutputStream fos = new FileOutputStream(resourceName)) {
+            fos.write(decodeResource(resourceAsString));
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
