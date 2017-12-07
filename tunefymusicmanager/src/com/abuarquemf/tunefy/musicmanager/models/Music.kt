37f24e5326a2d56bcfd3eac2cf5447fc5c6efb65
ABuarque <abuarquemf@gmail.com>
2017-12-07 13:13:26 -0300

    chanded model

--- tunefymusicmanager/src/com/abuarquemf/tunefy/musicmanager/models/Music.kt
+++ tunefymusicmanager/src/com/abuarquemf/tunefy/musicmanager/models/Music.kt
@@ -3,9 +3,9 @@ package com.abuarquemf.tunefy.musicmanager.models
 data class Music(val name: String, val band: String,
                  val musicResource: String, val id: Long) {
 
-    constructor() : this("music_name", "band_name", "music_resource", -505)
+    constructor() : this("music_name", "band_name", String(), -505)
 
     constructor(name: String, band: String, musicResource: String) : this(name, band, musicResource, -505)
 
-    constructor(id: Long): this("music_name", "band_name", "music_resource", id)
+    constructor(id: Long): this("music_name", "band_name", String(), id)
 }
