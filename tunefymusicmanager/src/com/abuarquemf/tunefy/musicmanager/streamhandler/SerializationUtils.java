d36a3af329785046d71b20b6cd901cd1c3bee241
ABuarque <abuarquemf@gmail.com>
2017-12-18 14:33:33 -0300

    added new class to handle string compression

---
+++ tunefymusicmanager/src/com/abuarquemf/tunefy/musicmanager/streamhandler/SerializationUtils.java
@@ -0,0 +1,43 @@
+package com.abuarquemf.tunefy.musicmanager.streamhandler;
+
+import java.io.*;
+import java.util.Base64;
+
+/**
+ * Utils for (de)serialization into Base64-encoded string for future persisting.
+ *
+ * @author andy (https://gist.github.com/andy722/1524968)
+ */
+class SerializationUtils {
+
+    public static <T extends Serializable> String serialize(T item) {
+        final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        final ObjectOutputStream objectOutputStream;
+        try {
+            objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+            objectOutputStream.writeObject(item);
+            objectOutputStream.close();
+            return Base64.getEncoder().encodeToString(byteArrayOutputStream.toByteArray());
+        } catch (IOException e) {
+            throw new Error(e);
+        }
+    }
+
+    public static <T extends Serializable> T deserialize(String data) {
+        try {
+            byte[] dataBytes = Base64.getDecoder().decode(data);
+            final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(dataBytes);
+            final ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+
+            @SuppressWarnings({"unchecked"})
+            final T obj = (T) objectInputStream.readObject();
+
+            objectInputStream.close();
+            return obj;
+        } catch (IOException e) {
+            throw new Error(e);
+        } catch (ClassNotFoundException e) {
+            throw new Error(e);
+        }
+    }
+}
